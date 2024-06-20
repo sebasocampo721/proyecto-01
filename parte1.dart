5fceb1178257b0cbb0ce1c64fcf1262397ed133a
sebasocampo721 <sebasocampo721>
2024-06-20 17:35:06 -0500

    se grego parte de distribucion de personas

--- parte1.dart
+++ parte1.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 void main(List<String> args) {
   int opcion;
@@ -14,41 +15,58 @@ void main(List<String> args) {
     if (opcion >= 1 && opcion < 5) {}
     switch (opcion) {
       case 1:
-        int cantTemas;
+        int cantGrupos;
         String temas, complejidad;
         List<String> tema = [];
-        List<String> complejidadTema = [];
+        List<String> dificultadTema = [];
         List<String> nombres = [];
         int cantNombres;
         String nombre;
         List<int> grupo = [];
         int contNombres;
-
-        print("ingrese la cantidad de temas ");
-        cantTemas = int.parse(stdin.readLineSync()!);
-        for (var i = 0; i < cantTemas; i++) {
+        String dificultad;
+        //-------------------------------------------------------------//
+        
+        print("ingrese la cantidad de grupos ");
+        cantGrupos = int.parse(stdin.readLineSync()!);
+        for (var i = 0; i < cantGrupos; i++) {
           print("ingrese el tema #${i + 1}");
           temas = stdin.readLineSync()!;
           tema.add(temas); //se agrega el tema a la lista
         }
-        for (var i = 0; i < cantTemas; i++) {
-          print("ingrese la complejidad del tema (baja-media-alta) #${i + 1}");
-          complejidad = stdin.readLineSync()!;
-          complejidadTema.add(complejidad); //se agrega el tema a la lista
+        for (var i = 1; i <= cantGrupos; i++) {
+        print('Ingrese la dificultad del tema $i (facil, medio, alto):');
+        dificultad = stdin.readLineSync()!;
+        if (dificultad != null && dificultad.isNotEmpty) {
+          dificultadTema.add(dificultad.toLowerCase());
         }
+      }
         print("ingrese la cantidad de alumnos");
         cantNombres = int.parse(stdin.readLineSync()!);
         for (var i = 0; i < cantNombres; i++) {
           print("ingrese el nombre #${i + 1}");
           nombre = stdin.readLineSync()!;
-          nombres.add(nombre); //se agrega el tema a la lista
+          nombres.add(nombre); //se agrega los nombres a la lista
         }
         print(nombres);
-        if (cantTemas == "baja") {
-          if (cantNombres < 2) {
-            print(cantNombres);
+
+        // Distribuir las personas en los grupos 
+        var grupos = <List<String>>[];
+        var personasRestantes = [...nombres]; 
+
+        for (var dificultad in dificultadTema) {
+          var grupo = <String>[];
+          var cantidadPersonasGrupo = (dificultad);
+
+          // Asegurar que el grupo tenga la cantidad correcta de personas
+          while (grupo.length < cantNombres && personasRestantes.isNotEmpty) {
+            var indiceAleatorio = Random().nextInt(personasRestantes.length);
+            grupo.add(personasRestantes.removeAt(indiceAleatorio));
           }
-        }
+
+          grupos.add(grupo);
+  }
+        
         break;
       case 2:
         break;
