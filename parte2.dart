2e7d35a56d111d2f5c935f4be597f5310acd926d
sebasocampo721 <sebasocampo721>
2024-06-22 09:30:42 -0500

    e corrigio error

--- parte2.dart
+++ parte2.dart
@@ -4,17 +4,17 @@ import 'dart:math';
 void main(List<String> args) {
   int opcion;
   List<List<String>> grupos = [];
-  //--------------------------------------------------------//
+
   do {
     print("Bienvenido a la app de creación de exposiciones");
     print("1. Crear grupos");
     print("2. Reorganizar grupos");
-    print("3. Utilizar datos guardados");
-    print("4. ");
+    print("3. Mostrar grupos");
+    print("4. Eliminar grupo");
     print("5. Salir");
     print("Digite la opción deseada:");
-    
-    opcion = int.parse(stdin.readLineSync()!);
+
+    opcion = int.tryParse(stdin.readLineSync()!) ?? 0;
 
     switch (opcion) {
       case 1:
@@ -28,9 +28,18 @@ void main(List<String> args) {
         }
         break;
       case 3:
+        if (grupos.isNotEmpty) {
+          mostrarGrupos(grupos);
+        } else {
+          print("No hay grupos creados todavía.");
+        }
         break;
       case 4:
-        print("");
+        if (grupos.isNotEmpty) {
+          eliminarGrupo(grupos);
+        } else {
+          print("No hay grupos creados todavía.");
+        }
         break;
       case 5:
         print("Adiós");
@@ -40,7 +49,6 @@ void main(List<String> args) {
     }
   } while (opcion != 5);
 }
-//-----------------------------------------------------------------------//
 
 List<List<String>> crearGrupos() {
   int cantGrupos;
@@ -49,9 +57,9 @@ List<List<String>> crearGrupos() {
   List<String> nombres = [];
   int cantNombres;
   String nombre;
-//--------------------------------------------------------------------//
+
   print("Ingrese la cantidad de grupos:");
-  cantGrupos = int.parse(stdin.readLineSync()!);
+  cantGrupos = int.tryParse(stdin.readLineSync()!) ?? 0;
 
   for (var i = 0; i < cantGrupos; i++) {
     print("Ingrese el tema del grupo #${i + 1}:");
@@ -64,7 +72,7 @@ List<List<String>> crearGrupos() {
   }
 
   print("Ingrese la cantidad de alumnos:");
-  cantNombres = int.parse(stdin.readLineSync()!);
+  cantNombres = int.tryParse(stdin.readLineSync()!) ?? 0;
 
   for (var i = 0; i < cantNombres; i++) {
     print("Ingrese el nombre #${i + 1}:");
@@ -72,7 +80,6 @@ List<List<String>> crearGrupos() {
     nombres.add(nombre);
   }
 
-  // Distribuir las personas en los grupos
   var grupos = <List<String>>[];
   var personasRestantes = [...nombres];
 
@@ -81,13 +88,11 @@ List<List<String>> crearGrupos() {
     var dificultad = dificultadTema[i];
     var cantidadPersonasGrupo = _calcularCantidadPersonas(dificultad);
 
-    // Asegurar que el grupo tenga al menos una persona
     if (personasRestantes.isNotEmpty) {
       var indiceAleatorio = Random().nextInt(personasRestantes.length);
       grupo.add(personasRestantes.removeAt(indiceAleatorio));
     }
 
-    // Llenar el grupo con el resto de personas necesarias
     while (grupo.length < cantidadPersonasGrupo && personasRestantes.isNotEmpty) {
       var indiceAleatorio = Random().nextInt(personasRestantes.length);
       grupo.add(personasRestantes.removeAt(indiceAleatorio));
@@ -96,17 +101,14 @@ List<List<String>> crearGrupos() {
     grupos.add(grupo);
   }
 
-  // Mostrar los grupos formados
   print('\nGrupos formados:');
   for (var i = 0; i < cantGrupos; i++) {
     print('Grupo ${i + 1}: ${grupos[i]}');
   }
 
   return grupos;
-  
 }
 
-    
 int _calcularCantidadPersonas(String dificultad) {
   switch (dificultad) {
     case 'facil':
@@ -130,18 +132,40 @@ void reorganizarGrupos(List<List<String>> grupos) {
 
   // Permitir al usuario seleccionar un grupo y reorganizarlo
   print("Ingrese el número de grupo que desea reorganizar (1-${grupos.length}):");
-  var grupoSeleccionado = int.parse(stdin.readLineSync()!) - 1;
+  var grupoSeleccionado = int.tryParse(stdin.readLineSync()!) ?? 0;
 
-  if (grupoSeleccionado >= 0 && grupoSeleccionado < grupos.length) {
-    print("Tema actual del Grupo ${grupoSeleccionado + 1}: ${grupos[grupoSeleccionado][0]}");
+  if (grupoSeleccionado > 0 && grupoSeleccionado <= grupos.length) {
+    print("Tema actual del Grupo ${grupoSeleccionado}: ${grupos[grupoSeleccionado - 1][0]}");
     print("Ingrese el nuevo tema para el grupo:");
     var nuevoTema = stdin.readLineSync()!;
 
-    grupos[grupoSeleccionado][0] = nuevoTema;
-    
-    print("Tema actualizado del Grupo ${grupoSeleccionado + 1}: $nuevoTema");
+    grupos[grupoSeleccionado - 1][0] = nuevoTema;
+
+    print("Tema actualizado del Grupo ${grupoSeleccionado}: $nuevoTema");
   } else {
     print("Número de grupo inválido.");
   }
-  
+}
+
+void mostrarGrupos(List<List<String>> grupos) {
+  print('\nGrupos existentes:');
+  for (var i = 0; i < grupos.length; i++) {
+    print('Grupo ${i + 1}: ${grupos[i]}');
+  }
+}
+
+void eliminarGrupo(List<List<String>> grupos) {
+  print("Eliminar Grupo:");
+
+  mostrarGrupos(grupos);
+
+  print("Ingrese el número de grupo que desea eliminar (1-${grupos.length}):");
+  var grupoSeleccionado = int.tryParse(stdin.readLineSync()!) ?? 0;
+
+  if (grupoSeleccionado > 0 && grupoSeleccionado <= grupos.length) {
+    var grupoEliminado = grupos.removeAt(grupoSeleccionado - 1);
+    print("Grupo $grupoSeleccionado: $grupoEliminado eliminado correctamente.");
+  } else {
+    print("Número de grupo inválido.");
+  }
 }
